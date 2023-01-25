db90cf25623580edd52da34eff52dfe09e56382a
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-25 17:39:19 +0530

    refactor : program to add multiple Address Book to the system Each address book has a unique name

--- src/com/bridgelabz/addressBookSystems/AddressBookSystem.java
+++ src/com/bridgelabz/addressBookSystems/AddressBookSystem.java
@@ -12,27 +12,27 @@ public class AddressBookSystem {
             System.out.println(
                     "1. Add New Contact\n2. Edit Contact\n3. Delete Contact" + "\n4. Display Contact\n5. Exit");
             System.out.println("Enter Choice: ");
-            Option = addressBook.scanner.nextInt();
+            Option = AddressBookCollection.userInput();
 
             switch (Option) {
                 case 1:
-                    addressBook.addContact();
+                    AddressBookCollection.addNewAddressBook();
                     break;
                 case 2:
-                    addressBook.showContacts();
+                    AddressBookCollection.deleteAddressBook();
                     break;
                 case 3:
-                    addressBook.editContact();
+                    AddressBookCollection.deleteAddressBook();
                     break;
                 case 4:
-                    addressBook.deleteContact();
+                    AddressBookCollection.selectAddressBook();
                     break;
                 default:
                     System.out.println(" !!!Invalid Input!!! ");
                     break;
 
             }
-        } while (Option != 4);
+        } while (Option != 5);
 
     }
 }
